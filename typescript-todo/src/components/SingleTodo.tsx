17393d6f57d073a79d4e57273c1653b96c79b0d5
nkemjikanma <nkemjikaohanyere@gmail.com>
2022-09-30 07:45:29 +0100

    fix: fix the done button on the todo list

--- typescript-todo/src/components/SingleTodo.tsx
+++ typescript-todo/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { edit, bin, done } from "../assets";
 import { Todo } from "../constants/model";
 
@@ -10,25 +10,54 @@ interface Props {
 }
 
 function SingleTodo({ index, todo, todos, setTodos }: Props) {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  function handleAction(idx: number, id: number) {
+    /**
+     * * idx === 2 => done was clicked
+     * * idx === 1 => delete was clicked
+     */
+    if (idx === 2) {
+      console.log(`You clicked done `);
+      setTodos(
+        todos.map((prevTodo) => {
+          return prevTodo.id === id
+            ? { ...prevTodo, isDone: !todo.isDone }
+            : todo;
+        })
+      );
+    }
+    if (idx === 1) {
+      // loop through the todos, and setTodos to every todo except the todo with the id
+      setTodos((previousTodos) => {
+        return previousTodos.filter((prevTodo) => prevTodo.id !== id);
+      });
+    }
+  }
+  const icons = [edit, bin, done];
   return (
     <form className="bg-todo-list mt-7 h-[50px] py-6 px-3 bg-slate-300 shadow-inner flex flex-row items-center justify-between rounded-md">
-      <h3 className="text-2xl font-thin">{todo.todo}</h3>
+      {todo.isDone ? (
+        <h3 className="text-2xl font-thin line-through text-slate-500">
+          {todo.todo}
+        </h3>
+      ) : (
+        <h3 className="text-2xl font-thin">{todo.todo}</h3>
+      )}
+
       <div className="flex flex-row gap-2">
-        <img
-          src={edit}
-          alt={"edit"}
-          className="h-7 rounded-sm hover:scale-[1.2] transition-all"
-        />
-        <img
-          src={bin}
-          alt={"edit"}
-          className="h-7 rounded-sm hover:scale-[1.2] transition-all"
-        />
-        <img
-          src={done}
-          alt={"edit"}
-          className="h-7 rounded-sm hover:scale-[1.2] transition-all"
-        />
+        {icons.map((icon, index) => {
+          return (
+            <img
+              key={index}
+              src={icon}
+              alt={index.toString()}
+              className="h-7 rounded-sm hover:scale-[1.1] transition-all hover:rotate-[360deg] cursor-pointer"
+              onClick={() => handleAction(index, todo.id)}
+            />
+          );
+        })}
       </div>
     </form>
   );
